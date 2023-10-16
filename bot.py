cc5668b66fa648d101eefe53ce03327a46ea0d2e
Alice Zhgir <alice.jgir@gmail.com>
2023-10-16 22:55:28 +0300

    Welcome message added

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import sys
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.types import Message
+from aiogram.filters import CommandStart
 from config import TOKEN
 
 dp = Dispatcher()
@@ -23,6 +24,14 @@ transliteration_dict = {
 
 def transliterate(text: str) -> str:
     return ''.join(transliteration_dict.get(char, char) for char in text)
+
+@dp.message(CommandStart())
+async def send_welcome(message: Message):
+    user_name = message.from_user.first_name
+    user_id = message.from_user.id
+    logging.info(f'{user_name=} {user_id=} sent message:{message.text}')
+    text = f'Привет, {user_name}! Я произведу транслитерацию для тебя. Присылай данные :)'
+    await message.reply(text)
 
 @dp.message()
 async def echo_handler(message: Message):
